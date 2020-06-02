a29cb5636d796163987304c76125833d8c664fc0
davidacealgo15 <davidacevedo115@gmail.com>
2020-06-02 03:42:53 -0500

    Setup server configurations

---
+++ .eslintrc.js
@@ -0,0 +1,42 @@
+module.exports = {
+    "env": {
+        "node": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": [
+        "eslint:recommended",
+        "plugin:react/recommended"
+    ],
+    "globals": {
+        "Atomics": "readonly",
+        "SharedArrayBuffer": "readonly"
+    },
+    "parserOptions": {
+        "ecmaFeatures": {
+            "jsx": true
+        },
+        "ecmaVersion": 11
+    },
+    "plugins": [
+        "react"
+    ],
+    "rules": {
+        "indent": [
+            "error",
+            "tab"
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],
+        "quotes": [
+            "error",
+            "single"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
--- app.js
+++ app.js
@@ -1,26 +1,31 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var app = express(),
+createError = require('http-errors'),
+cookieParser = require('cookie-parser'),
+express = require('express'),
+logger = require('morgan'),
+path = require('path'),
+port = process.env.PORT || 3000;
 
 var indexRouter = require('./routes/index');
+var toDoRouter = require('./routes/toDo');
 var usersRouter = require('./routes/users');
 
-var app = express();
-
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
+app.set('views', path.join(__dirname, '/views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
 app.use(express.json());
-app.use(express.urlencoded({ extended: false }));
+app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
-
+app.use(express.static(path.join(__dirname, '/public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use("/toDo", toDoRouter);
+
+app.get("/", function(req, res) {
+  res.sendFile("index.html");
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
@@ -37,5 +42,7 @@ app.use(function(err, req, res, next) {
   res.status(err.status || 500);
   res.render('error');
 });
+
+app.listen(port, () => console.log("Server listening in port: " + port));
 
 module.exports = app;
